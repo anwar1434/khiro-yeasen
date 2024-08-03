ca05ccc132f39b33aca0d31a987a20f4cd5709f8
anwar1434 <anwarobeid75@gmail.com>
2024-08-03 21:18:22 +0300

    Update seting.js

--- router/seting.js
+++ router/seting.js
@@ -2,37 +2,54 @@ import express from "express";
 import { StudentInfo } from "../models/student.js";
 const router = express.Router();
 
-router.get("/:StatisticsType", async (request, response) => {
+router.get( "/:StatisticsType", async ( request, response ) =>
+{
     const StatisticsType = request.params.StatisticsType;
-    try {
+    try
+    {
         let studentsStatistics = "";
         let title = "";
-        if (StatisticsType === "احصائيات الحضور") {
+        if ( StatisticsType === "احصائيات الحضور" )
+        {
             studentsStatistics = await StudentInfo.find( {} ).sort( { "daysOfAttendance.attendance.total": -1 } );
             title = "عدد أيام الحضور"
-            
+
         }
 
-        else if ( StatisticsType === "احصائيات التسميع" )
-        {
-            studentsStatistics = await StudentInfo.find( {} ).sort( { "pagesOfRecitation.total": -1 } );
+        else if (StatisticsType === "احصائيات التسميع ( جديدة )") {
+            studentsStatistics = await StudentInfo.aggregate([
+                {
+                    $addFields: {
+                        newPagesLength: { $size: "$pagesOfRecitation.newPages" }
+                    }
+                },
+                {
+                    $sort: { newPagesLength: -1 }
+                }
+            ]);
         }
-            
-        else if ( StatisticsType === "معلومات ناقصه" )
-        {
-            studentsStatistics = await StudentInfo.find({
-                $or: [
-                    { father: '' },
-                    { phoneNumber: 0 },
-                ]
-            });
+
+        else if (StatisticsType === "احصائيات التسميع ( قديمة )") {
+            studentsStatistics = await StudentInfo.aggregate([
+                {
+                    $addFields: {
+                        oldPagesLength: { $size: "$pagesOfRecitation.oldPages" }
+                    }
+                },
+                {
+                    $sort: { oldPagesLength: -1 }
+                }
+            ]);
         }
 
         response.status( 200 ).json( { data: studentsStatistics } );
 
-    } catch (error) {
-        response.status(500).json({ message: "يوجد خطأ في الاتصال الرجاء المحاولة لاحقا" });
+    } catch ( error )
+    {
+        response.status( 500 ).json( { message: "يوجد خطأ في الاتصال الرجاء المحاولة لاحقا" } );
     }
-});
+} );
 
-export default router;
+export default router
+
+
